46169bfa05dd1d8b113617228f59bf7a21917490
Emil Tomov <bemcho@gmail.com>
2019-08-03 16:42:14 +0300

    Clean up

--- random-search.jl
+++ random-search.jl
@@ -1,11 +1,13 @@
-using StatsBase
+using StatsBase: sample 
+
+globalBest = Dict(:cost => rand(0.1:0.1:1), :vector => [])
 
 function objective_function(vector)
     return sum(vector.^2)
 end
 
 function random_vector(searchSpace::AbstractArray)
-    return rand(minimum(searchSpace)[1]:0.1:maximum(searchSpace[1]), 2)
+    return [sample(searchSpace[i],1)[1] for i in 1:first(size(searchSpace))]
 end
 
 function search(search_space, max_iter)
@@ -14,7 +16,12 @@ function search(search_space, max_iter)
         candidate = Dict()
         candidate[:vector] = random_vector(search_space)
         candidate[:cost] = objective_function(candidate[:vector])
-        if (best === nothing || candidate[:cost] < best[:cost]) best = candidate end
+        if (best === nothing || candidate[:cost] < best[:cost]) 
+            best = candidate
+            if best[:cost] < globalBest[:cost]
+                global globalBest = best
+            end
+        end
         println(" > iteration=$(iter + 1), best=$(best[:cost])")
     end
     return best
@@ -22,9 +29,9 @@ end
 
 # problem configuration
 problem_size = 2
-search_space = [[x for x in -5.0:0.1:5.0],[x for x in -5.0:0.1:5.0]]
+search_space = [collect(-5.0*i:0.1/i^2:5.0*i) for i in 1:problem_size]
 # algorithm configuration
-max_iter = 1000
+max_iter = 100
 # execute the algorithm
 best = search(search_space, max_iter)
-println("Done.\n Best Solution: c=$(best[:cost]), v=$(best[:vector])")
+println("Done.\n Best Solution: c=$(best[:cost]), v=$(best[:vector])\n GlobalBestScoreForAllRuns: c=$(globalBest[:cost]),v=$(globalBest[:vector]) ")
